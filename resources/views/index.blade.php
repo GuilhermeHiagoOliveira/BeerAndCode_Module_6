2d73ebdaea120cc8ad9beb9302c3e61212684430
Guilherme <gui.hiago.oliveira@gmail.com>
2024-12-03 11:46:06 -0300

    Blade directives - HTML attributes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,71 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
+
+    <style>
+        .even {
+            color: #34e313;
+            font-weight: bold;
+        }
+        .odd {
+            color: #e31313;
+        }
+
+        .text {
+            font-weight: bold;
+            font-size: 10px;
+        }
+    </style>
 </head>
 <body>
 
-    @for($i = 0; $i < 10; $i++)
-        Index: {{ $i }} <br>
-    @endfor
-
-    <br>
-
-    @foreach ($users as $user)
-        Usuário: {{ $user['name'] }} <br>
-    @endforeach
-
-    <br>
-
-    @forelse ($users2 as $user)
-        Usuário: {{ $user['name'] }} <br>
-    @empty
-        Lista vazia
-    @endforelse
-
-    <br>
-
-    @while ($i < 20)
-        Index: {{ $i }} <br>
-        @php
-            $i++;
-        @endphp
-    @endwhile
-
-    <br>
-
     @foreach($users as $user)
-        Index: {{ $loop->index }} <br>
-        Iteration: {{ $loop->iteration }} <br>
-        Remaining: {{ $loop->remaining }} <br>
-
-        @if($loop->first)
-            Primeiro: {{ $user['name'] }} <br>
-        @endif
-
-        @if($loop->even)
-            Par: {{ $user['name'] }} <br>
-        @endif
-
-        @if($loop->odd)
-            Ímpar: {{ $user['name'] }} <br>
-        @endif
-
-        @if($loop->last)
-            Último: {{ $user['name'] }} <br>
-        @endif
-
-        {{ $user['name'] }} <br>
+        <span @class(['text','even' => $loop->even, 'odd' => $loop->odd])>
+            {{ $user['name'] }} <br>
+        </span>
 
         <br>
+
+        <label>
+            <input name="checkbox" type="checkbox" @checked(false) />
+            {{  $user['name'] }}
+        </label><br>
     @endforeach
 
     <br>
 
-    @foreach($users as $user)
+    <select @disabled(true)>
+        <option value="">Select User</option>
         @foreach($users as $user)
-            Iteração do loop pai: {{ $loop->parent->iteration }} <br>
+            <option @selected($user['id'] == 2) value="{{ $user['id'] }}">{{ $user['name'] }}</option>
         @endforeach
-    @endforeach
+    </select>
+
+    <input type="text" @readonly(true) value="Hello World" />
 
 
 </body>
